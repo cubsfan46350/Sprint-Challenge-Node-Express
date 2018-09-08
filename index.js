d9b89dfcb6602d4aa02a4bbe103b9da69f00b6f9
cubsfan46350 <cubsfan.46350@msn.com>
2018-09-08 02:57:20 -0500

    completed sprint

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const server = express();
-const projectsDb = require('./data/helpers/projectModel.js');;
+const projectsDb = require('./data/helpers/projectModel.js');
+const actionsDb = require('./data/helpers/actionModel.js');
 //MiddleWare to Use
 server.use(express.json());
 
@@ -37,8 +38,36 @@ server.delete('/api/projects/:id', (req, res) => {
         .catch(err => res.status(500).json(err));
 });
 
+server.get('/api/actions', (req, res) => {
+    actionsDb.get()
+        .then(actions => {
+            res.status(200).json(actions);
+        })
+        .catch(err => {
+            console.log('error', err);
+            res.status(500).json({ message: 'Cannot retrieve action data' });
+        });
+});
 
+server.post('/api/actions', (req, res) => {
+    const add = req.body;
+    actionsDb.insert(add)
+        .then(res => res.status(201).json(res))
+        .catch(err => res.status(500).json(err));
+});
 
+server.put('/api/actions/:id', (req, res) => {
+    actionsDb.update(req.params.id, req.body)
+        .then(actions => res.status(200).json(actions))
+        .catch(err => res.status(500).json(err));
+});
+
+server.delete('/api/actions/:id', (req, res) => {
+    const id = req.params.id;
+    actionsDb.remove(id, req.body)
+        .then(res => res.status(200).json(res))
+        .catch(err => res.status(500).json(err));
+});
 
 
 server.listen(3000, () => console.log('Listening on Port 3000'));
